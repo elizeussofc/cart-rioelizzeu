bbaa883a8cf02ce2b739e5ed9d72b7030a1d5e27
elizeussofc <elizeu.santos21oficial@gmail.com>
2024-07-22 15:29:58 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário.

--- cartorio elizzeu ss.c
+++ cartorio elizzeu ss.c
@@ -125,7 +125,8 @@ int deletar()
    printf("Escolha a opção desejada do menu\n\n");
    printf("\t1 - Registrar nomes\n");
    printf("\t2 - Consultar nomes\n");
-   printf("\t3 - Deletar nomes\n\n");
+   printf("\t3 - Deletar nomes\n");
+   printf("\t4 - sair do sistema\n\n");
    printf("Opção: ");//fim de menu
    
    scanf("%d", &opcao); //armazenamento a escolha do usuário
@@ -145,6 +146,11 @@ int deletar()
    	   		
    	   	case 3:
    	   	deletar();
+   	   	break;
+   	   	
+   	   	case 4:
+   	   	printf("Obrigado por utilizar o sistema!\n");
+   	   	return 0;
    	   	break;
    	   			
    	   	default:
